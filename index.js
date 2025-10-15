2a9b15497569176e580800f4318fb5cb6554f9bd
polluste888 <bert-robert.polluste@voco.ee>
2025-10-15 15:10:57 +0300

    java

---
+++ index.js
@@ -0,0 +1,33 @@
+//console.log("Hello");
+//console.log("i like pizza!");
+
+//window.alert("this is an alert!");
+//window.alert("i like pizza!")
+
+document.getElementById(`myh1`).textContent = `Hello`;
+document.getElementById(`myP`).textContent = `i like pizza`;
+// this is comment
+
+/*
+this
+is
+a
+comment
+*/
+//variable = a container that stores a value.
+//behaves as if it were the values it cont
+
+//1. declaration let x;
+//2. assignemt x= 100;
+let x;
+x=100;
+let age=25;
+let price=10.99;
+let gpa = 3;
+console.log(age);
+console.log(price);
+console.log(3);
+
+console.log(`you are ${age} years old`);
+console.log(`the rpice is $${price}`);
+console.log(`your gpa is: ${gpa}`);
